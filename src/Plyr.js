669ff4c063532433254ae2a58b761469a7fbcc14
Fabien Foerster <fabienfoerster@gmail.com>
2016-07-28 23:51:48 +0200

    Mom I love you

---
+++ src/Plyr.js
@@ -0,0 +1,40 @@
+import React, { Component } from 'react';
+import plyr from 'plyr';
+import './Plyr.css';
+
+
+class Plyr extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {currentTime: '', playing: false};
+    this.handleTimeUpdate = this.handleTimeUpdate.bind(this);
+    this.handlePlayerStateUpdate = this.handlePlayerStateUpdate.bind(this);
+  }
+  componentDidMount() {
+    let player = plyr.setup(this.refs.plyr)[0];
+    player.addEventListener('timeupdate', this.handleTimeUpdate);
+    player.addEventListener('play',this.handlePlayerStateUpdate);
+    player.addEventListener('pause',this.handlePlayerStateUpdate);
+  }
+  handleTimeUpdate(e) {
+    this.setState({currentTime: e.target.plyr.media.currentTime});
+  }
+  handlePlayerStateUpdate(e) {
+    this.setState({playing: e.type === "play" ? true : false});
+  }
+  render() {
+    return (
+      <div>
+      <div class="plyr" ref="plyr">
+        <div data-video-id="bTqVqk7FSmY" data-type="youtube">
+        </div>
+
+      </div>
+      <h5>Current time : {this.state.currentTime}</h5>
+    <h5>Playing :  {this.state.playing.toString()}</h5>
+    </div>
+    );
+  }
+}
+
+export default Plyr;
